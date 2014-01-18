c5ec29939b43f0456953c3da1a5807adb22c4f1f
Concetto Condorelli <concettocondorelli@gmail.com>
2014-01-18 22:20:15 +0100

    Added signup using cookies to blog app - Problem Set 4

--- blog.py
+++ blog.py
@@ -1,14 +1,34 @@
 import webapp2
 
 from google.appengine.ext import db
-from common import Handler
+from common import BaseHandler
+
+import re
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+PWD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PWD_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
 
 class Post(db.Model):
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
-class MainPage(Handler):
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    password = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+class MainPage(BaseHandler):
     def render_front(self):
         posts = db.GqlQuery("SELECT * FROM Post "
                             "ORDER BY created DESC ")
@@ -18,7 +38,7 @@ class MainPage(Handler):
     def get(self):
         self.render_front()
 
-class NewPost(Handler):
+class NewPost(BaseHandler):
     def render_page(self, subject="", content="", error=""):
         self.render("blog/newpost.html", subject=subject, content=content, error=error)
 
@@ -38,7 +58,7 @@ class NewPost(Handler):
             error = "Subject and content are required!"
             self.render_page(subject, content, error)
 
-class Permalink(Handler):
+class Permalink(BaseHandler):
     def render_page(self, post_id):
         post = Post.get_by_id(int(post_id))
         self.render("blog/front.html", posts=[post])
@@ -46,8 +66,66 @@ class Permalink(Handler):
     def get(self, post_id):
         self.render_page(post_id)
 
+class Signup(BaseHandler):
+    def render_page(self, username="", email="", username_error="", password_error="", verify_error="", email_error=""):
+        self.render("blog/signup.html", username=username, email=email, 
+                    username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
+
+    def get(self):
+        self.render_page()
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        username_error = ""
+        password_error = ""
+        verify_error = ""
+        email_error = ""
+
+        if not valid_username(username):
+            username_error = "That's not a valid username."
+
+        if password == verify:
+            if not valid_password(password):
+                password_error = "That wasn't a valid password."
+        else:
+            verify_error = "Your passwords didn't match."      
+        
+        if email and not valid_email(email):
+            email_error = "That's not a valid email."
+
+        if username_error or password_error or verify_error or email_error:
+            self.render_page(username, email, username_error, password_error, verify_error, email_error)
+        else:
+            user = User(username=username, password=password, email=email)
+            user.put()
+
+            self.set_secure_cookie('user_id', str(user.key().id()))
+            self.redirect("/blog/welcome")
+
+
+class WelcomePage(BaseHandler):
+
+    def get(self):
+        cookie_val = self.read_secure_cookie('user_id')
+
+        if cookie_val:
+            user_id = int(cookie_val)
+            user = User.get_by_id(user_id)
+            
+            username = user.username
+            self.render("blog/welcome.html", username=username)
+        else:
+            self.set_secure_cookie('user_id','')
+            self.redirect("/blog/signup")
+
 
 app = webapp2.WSGIApplication([(r'/blog/?', MainPage),
                                (r'/blog/newpost', NewPost),
-                               (r'/blog/(\d+)', Permalink)
+                               (r'/blog/(\d+)', Permalink),
+                               (r'/blog/signup', Signup),
+                               (r'/blog/welcome', WelcomePage)
                                ], debug = True)
--- common.py
+++ common.py
@@ -6,7 +6,24 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-class Handler(webapp2.RequestHandler):
+
+import hmac
+
+SECRET = 'imsosecret'
+
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    s = h.split('|')[0]
+    if h == make_secure_val(s):
+        return s
+
+
+class BaseHandler(webapp2.RequestHandler):
     def write(self, *args, **kwargs):
         self.response.out.write(*args, **kwargs)
 
@@ -16,4 +33,15 @@ class Handler(webapp2.RequestHandler):
 
     def render(self, template, **kwargs):
         self.write(self.render_str(template, **kwargs))
+
+    def set_secure_cookie(self, name, value):
+        secure_cookie = ''
+        if value:
+            secure_cookie = make_secure_val(value)
+        self.response.headers.add_header('Set-Cookie', '%s=%s;Path=/' % (name, secure_cookie))
+
+
+    def read_secure_cookie(self, name):
+        secure_cookie = self.request.cookies.get(name)
+        return secure_cookie and check_secure_val(secure_cookie)
 
